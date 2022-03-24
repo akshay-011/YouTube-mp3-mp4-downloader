1b702273548378feaccd200a58bc277fc1cf6649
akshay-011 <akshaykumarkannan2003826@gmail.com>
2022-03-24 19:53:43 +0530

    prototype

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, send_file
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/yt2mp3")
+def yt2mp3():
+    return render_template("yt2mp3.html")
+
+@app.route("/yt2mp4")
+def yt2mp4():
+    return render_template("yt2mp4.html")
+
+@app.route("/download-mp3", methods=['POST'])
+def download_mp3():
+    from main import download
+    link = request.form.get("link")
+    name = download.download_mp3(link)
+    return send_file(name, as_attachment=True)
+
+@app.route("/download-mp4", methods=["POST"])
+def download_mp4():
+    link = request.form.get('link')
+    resolution = request.form.get("quality")
+    from main import download
+    name = download.download_mp4(link=link, resulution=resolution)
+    return send_file(name, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,29 @@
+class download(object):
+    def __init__(self):
+        pass
+    @staticmethod   
+    def download_mp3(link):
+        import os
+        from pytube import YouTube
+        path = os.path.join(os.getcwd(), "medias/")
+        mp3 = YouTube(link)
+        name = mp3.title + ".mp3"
+        file = mp3.streams.get_audio_only()
+        file.download(filename=name, output_path=path)
+        return os.path.join(path, name)
+    @staticmethod
+    def download_mp4(link, resulution):
+        import os
+        from pytube import YouTube
+        
+        mp4 = YouTube(link)
+        path = os.path.join(os.getcwd(), "medias/")
+        name = mp4.title + ".mp4"
+        if resulution == "high":
+            file = mp4.streams.get_highest_resolution()
+        elif resulution == "low":
+            file = mp4.streams.get_lowest_resolution()
+        else:
+            file = mp4.streams.filter(file_extension='mp4')
+        file.download(filename=name, output_path=path)
+        return os.path.join(path, name)
